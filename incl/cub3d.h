687a1c0c2146cd19f142b77bd0a8a3cf60b9ecfc
Laura de Koning <lade-kon@student.codam.nl>
2025-03-07 17:38:53 +0100

    end of day push

--- incl/cub3d.h
+++ incl/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/26 13:05:32 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/06 20:29:09 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/07 15:49:48 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@
 
 
 void	*safe_calloc(size_t count, size_t size);
-// void	error_message(char *str);
+void	error_message(char *str);
+char	*read_file(const char *file);
 // void	free_data(t_data *data);
 // void	get_data(t_data *data, const char *file);
-// char	*read_file(const char *file);
 // void	validate_file(char *file_str);
 // void	parse_data(t_data *data, char *file_str);
 // void	init_data(t_data *data, char *file_str);
--- src/get_data.c
+++ src/get_data.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/06 18:23:48 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/06 19:39:56 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/07 15:56:08 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	get_data(t_data *data, const char *file)
 {
-	char		*file_as_str;
+	char	*file_as_str;
 
 	file_as_str = read_file(file);
-	validate_filecontent(file_as_str);
+	validate_file_and_init_data(file_as_str);
 	parse_data(data, file_as_str);
 	init_data(data, file_as_str);
 }
