1c5991a181725dd4ef6f772ee2d2f981f54c0b73
Laura de Koning <laura-dekoning@live.nl>
2025-03-11 21:07:23 +0100

    Corrected ft_split values and pointer to player in set_default.c

--- src/set_default.c
+++ src/set_default.c
@@ -9,8 +9,8 @@ void	set_default(t_data *data)
 	data->west_texture = ft_strdup(DEF_WEST);
 	data->floor = string_to_rgb(DEF_FLOOR);
 	data->ceiling = string_to_rgb(DEF_CEILING);
-	data->map = ft_split(DEF_MAP);
-	data->player.x = 5;
-	data->player.y = 2;
-	data->player.dir = SOUTH;
+	data->map = ft_split(DEF_MAP, '\n');
+	data->player->x = 5;
+	data->player->y = 2;
+	data->player->dir = SOUTH;
 }
