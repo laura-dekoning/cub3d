aed3303fed6c4d7ac01244eb705587e6ee29794d
Laura de Koning <laura-dekoning@live.nl>
2025-03-11 21:05:42 +0100

    Added set_default.c to set default data struct in case values are missing in the file.

---
+++ src/set_default.c
@@ -0,0 +1,16 @@
+
+#include "cub3d.h"
+
+void	set_default(t_data *data)
+{
+	data->north_texture = ft_strdup(DEF_NORTH);
+	data->south_texture = ft_strdup(DEF_SOUTH);
+	data->east_texture = ft_strdup(DEF_EAST);
+	data->west_texture = ft_strdup(DEF_WEST);
+	data->floor = string_to_rgb(DEF_FLOOR);
+	data->ceiling = string_to_rgb(DEF_CEILING);
+	data->map = ft_split(DEF_MAP);
+	data->player.x = 5;
+	data->player.y = 2;
+	data->player.dir = SOUTH;
+}
