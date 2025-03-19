e94b2377ea1aef38e54f34c15afeff5091aad245
Laura de Koning <lade-kon@student.codam.nl>
2025-03-19 16:00:20 +0100

    IMPORTANT: still have to fix making map. Do we want to add all blank spaces from the file? Need to make sure the map end after last line with walls.

--- src/validate_file_and_init_data.c
+++ src/validate_file_and_init_data.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/11 18:22:48 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/19 14:23:34 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/19 15:49:49 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	validate_file_and_init_data(t_data *data, char *file_as_str)
 	while (file_as_str[start] != '\0')
 	{
 		print_data(data);
+		while (file_as_str[start] == '\n' || file_as_str[start] == ' ')
+			start++;
 		if (is_setting(file_as_str, start) == PATH)
 			start = parse_validate_init_textures(data, file_as_str, start);
 		else if (is_setting(file_as_str, start) == RGB)
