70197fe5862b9dfc7f413bd1fefad85c1bf5fbeb
Laura de Koning <lade-kon@student.codam.nl>
2025-03-28 13:20:41 +0100

    Added safety checks for allocating memory in alloc_mem_for_data

--- src/alloc_mem_for_data.c
+++ src/alloc_mem_for_data.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/13 14:11:04 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/20 18:40:24 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/28 13:20:17 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,16 @@ void	*alloc_mem_for_data(void)
 	t_data		*data;
 
 	data = (t_data *)safe_calloc(NULL, 1, sizeof(t_data));
+	if (!data)
+		error_message(data, MALLOC);
 	data->map = (t_map *)safe_calloc(data, 1, sizeof(t_map));
+	if (!data->map)
+		error_message(data, MALLOC);
 	data->player = (t_player *)safe_calloc(data, 1, sizeof(t_player));
+	if (!data->player)
+		error_message(data, MALLOC);
 	data->check = (t_check *)safe_calloc(data, 1, sizeof(t_check));
+	if (!data->check)
+		error_message(data, MALLOC);
 	return (data);
 }
