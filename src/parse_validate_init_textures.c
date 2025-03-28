ff8ee6245049484c2a18cd73949e280c9c74d6bb
Laura de Koning <lade-kon@student.codam.nl>
2025-03-28 15:59:16 +0100

    Fixed: deleted start int in parse_validate_init_textures. Looks like function is fine now to work with a single str/line from the file_as_arr

--- src/parse_validate_init_textures.c
+++ src/parse_validate_init_textures.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/28 15:27:18 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/27 07:59:06 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/28 15:58:21 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,60 @@
 
 //Maybe change variables. Maybe pass whole data struct to EVERY function for clean exit everywhere
 //Also no hassle with pointers and stuff. 
-size_t	set_texture(char **target, char *line, size_t start)
+void	set_texture(char **target, char *line)
 {
 	size_t	len;
 
-	len = start;
-	while(line[len] != '\0' && line[len] != ' ' && line[len] != '\n')
+	len = 3;
+	while(line[len] != '\0' && line[len] != ' ')
 		len++;
 	if (*target)
 		free(*target);
-	*target = ft_substr(line, start, (len - start));
-	while (line[len] == ' ' || line[len] == '\n')
+	*target = ft_substr(line, 3, len);
+	while (line[len] == ' ')
 		len++;
-	return (len);
 }
 
 //Maybe make the textures in a char ** so it can align with the enums 
-size_t	init_texture_data(t_data *data, char *line, size_t start, int flag)
+void	init_texture_data(t_data *data, char *line, int flag)
 {
-	size_t	new_start;
-
-	new_start = 0;
 	if (data->check->setting[flag] == true)
 		error_message(data, DUP_TEXTURE);		
 	if (flag == NORTH)
-		new_start = set_texture(&data->north_texture, line, start + 3);
+		set_texture(&data->north_texture, line);
 	else if (flag == EAST)
-		new_start = set_texture(&data->east_texture, line, start + 3);
+		set_texture(&data->east_texture, line);
 	else if (flag == SOUTH)
-		new_start = set_texture(&data->south_texture, line, start + 3);
+		set_texture(&data->south_texture, line);
 	else if (flag == WEST)
-		new_start = set_texture(&data->west_texture, line, start + 3);
+		set_texture(&data->west_texture, line);
 	data->check->setting[flag] = true;
-	return (new_start);
 }
 
-int	validate_texture_id(t_data *data, char *line, size_t start)
+int	validate_texture_id(t_data *data, char *line)
 {
 	int	flag;
 
 	flag = 0;
-	if (ft_strncmp(&line[start], "NO ", 3) == 0)
+	if (ft_strncmp(line, "NO ", 3) == 0)
 		flag = NORTH;
-	else if (ft_strncmp(&line[start], "EA ", 3) == 0)
+	else if (ft_strncmp(line, "EA ", 3) == 0)
 		flag = EAST;
-	else if (ft_strncmp(&line[start], "SO ", 3) == 0)
+	else if (ft_strncmp(line, "SO ", 3) == 0)
 		flag = SOUTH;
-	else if (ft_strncmp(&line[start], "WE ", 3) == 0)
+	else if (ft_strncmp(line, "WE ", 3) == 0)
 		flag = WEST;
 	else
 		error_message(data, "Invalid texture ID\n");
 	return (flag);
 }
 
-size_t	parse_validate_init_textures(t_data *data, char *line, size_t start)
+void	parse_validate_init_textures(t_data *data, char *line)
 {
 	int		flag;
-	size_t	new_start;
 
 	if (data->check->setting[MAP] == true)
 		error_message(data, "File should end with map content!");
-	new_start = 0;
-	flag = validate_texture_id(data, line, start);
-	new_start = init_texture_data(data, line, start, flag);
-	return (new_start);
+	flag = validate_texture_id(data, line);
+	init_texture_data(data, line, flag);
 }
--- src/string_to_rgb.c
+++ src/string_to_rgb.c
@@ -6,7 +6,7 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/06 19:52:29 by lade-kon      #+#    #+#                 */
-/*   Updated: 2025/03/28 15:45:04 by lade-kon      ########   odam.nl         */
+/*   Updated: 2025/03/28 15:54:14 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,11 @@ static void	is_correct_rgb(t_data *data, char c)
 		error_message(data, NEG);
 }
 
-void	string_to_rgb(t_data *data, char *str, int i)
+int	*string_to_rgb(t_data *data, char *str, int i)
 {
 	int	n;
 	int	diff;
+	int	*rgb;
 
 	n = 0;
 	while (n < 3)
@@ -57,7 +58,7 @@ void	string_to_rgb(t_data *data, char *str, int i)
 // 	rgb = string_to_rgb(int_str, 0);
 // 	while (n < 3)
 // 	{
-// 		printf("rbg[%i] = %i\n", n, rgb[n]);
+// 		printf("rgb[%i] = %i\n", n, rgb[n]);
 // 		n++;
 // 	}
 // 	return (0);
